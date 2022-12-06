ef9305624f532307bf148fcc245d191e769d2a15
Tomasz Plonsk <tplonski@luxoft.com>
2022-12-06 12:40:42 +0100

    working kafka with producer

---
+++ gameOperator/src/main/java/com/tplonski/gameOperator/GameOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.tplonski.gameOperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOperatorApplication.class, args);
+	}
+
+}
---
+++ gameOperator/src/test/java/com/tplonski/gameOperator/GameOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tplonski.gameOperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
